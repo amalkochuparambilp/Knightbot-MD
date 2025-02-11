6c8e2393c18ebc4f43178dc91eefdad14e96ad85
amalkochuparambilp <98939651+amalkochuparambilp@users.noreply.github.com>
2025-02-12 02:12:43 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'AKP',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "AKP.AI OLD VER",
+  botOwner: 'Amal', // Your name
+  ownerNumber: '917510211318', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
